db4c0ee38c2a87f3fa703b5c235665d5164283f0
Balganym <aiho_2005@mail.ru>
2018-01-10 14:11:53 +0600

    MVC

---
+++ Models/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+		private static List<GuestResponse> responces = new List<GuestResponse>();
+		public static IEnumerable<GuestResponse> Responses { get { return responces; } }
+		public static void AddResponse(GuestResponse response)
+		{
+			responces.Add(response);
+		}
+
+	}
+}
